b328a599b057364335782e34f01b50361f8cd3fc
Raghav <raghavdahal686@gmail.com>
2024-07-10 12:05:19 +0545

    assets added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
+import LandingPage from "./components/LandingPage"
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">Hello World</h1>
-      <p>this is my first react project</p>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+      </Routes>
+    </Router>
     </>
   )
 }
